69482889a360f865d5f2b0a02175b75aab001db6
Nicole Lucien <nicolemlucien@gmail.com>
2019-10-25 03:06:00 +0000

    Done.

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,1 +1,17 @@
-# Your code goes here!
+require 'pry'
+
+class Dog 
+
+  def name=(dog_name)
+  @chosen_name = dog_name
+  end
+  
+  def name 
+  @chosen_name
+  end
+  
+  def bark 
+  puts "woof!"
+  end
+
+end
